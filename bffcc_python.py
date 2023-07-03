bca6bb18a6f17428db08e071e97181ed89d14d5c
mnmartinelli <michaelnmartinelli@gmail.com>
2023-07-03 00:51:58 -0400

    caesar cipher decrypt working

--- bffcc_python.py
+++ bffcc_python.py
@@ -1,1 +1,27 @@
-# Your string to brute force "Ugew gnwj zwjw Oslkgf"
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# Your string to brute force "Ugew gnwj zwjw Oslkgf"    
+ciphertext = "Ugew gnwj zwjw Oslkgf"
+
+def decrypt(n, ciphertext):
+    result = ''
+    for l in ciphertext:
+        try:
+                if l.isupper():
+                    index= ALPHABET.index(l)
+                    i = (index - n) % 26
+                    result += ALPHABET[i]
+
+                else:
+                    index = alphabet.index(l)
+                    i = (index - n) % 26
+                    result += alphabet[i]
+        except ValueError:
+            result += l
+    return result
+
+#brute force while for loop testing every shift combo
+for key in range(26):
+    dec = decrypt(key, ciphertext)
+    print("%d . %s" % (key,dec))
